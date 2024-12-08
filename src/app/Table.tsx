303640a0e3a4f679b711111ee9cf30804cea9ec8
AugustoConti <augustoconti10@gmail.com>
2024-12-08 02:03:11 -0300

    Added link to GH

--- src/app/Table.tsx
+++ src/app/Table.tsx
@@ -5,6 +5,8 @@ import {
   ChevronDown,
   CircleDotDashed,
   Copy,
+  Club,
+  Diamond,
   Heart,
   Spade,
   Link as LinkIcon,
@@ -31,6 +33,17 @@ import { Table, type PokerTable } from "@/models/table";
 import AddPlayerForm from "./AddPlayerForm";
 import CashOutForm from "./CashOutForm";
 import ReBuyForm from "./ReBuyForm";
+
+function GitHub() {
+  return (
+    <svg className="size-6 text-white" role="img" viewBox="0 0 24 24">
+      <path
+        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
 
 function TableBackground({ children }: { children: ReactNode }) {
   return (
@@ -128,7 +141,7 @@ function TableComponent({ table, resetTable }: { table: PokerTable; resetTable?:
   const tableUrl = () => `${window.location.origin}/${table.getId()}`;
 
   const router = useRouter();
-  const pushToHome = async () => router.push("/");
+  const pushToHome = () => router.push("/");
 
   const [showQRLink, setShowQRLink] = useState(false);
   const [linkCopied, setLinkCopied] = useState(false);
@@ -170,7 +183,7 @@ function TableComponent({ table, resetTable }: { table: PokerTable; resetTable?:
         ))}
       </TableBackground>
       <div className="space-y-4 p-4">
-        <header className="flex items-center justify-between">
+        <header className="flex flex-col items-center justify-between space-y-2 sm:flex-row">
           <h1 className="text-2xl font-semibold">
             <Spade className="mr-2 inline-block h-6 w-6" fill="currentColor" />
             Pokereros
@@ -179,7 +192,7 @@ function TableComponent({ table, resetTable }: { table: PokerTable; resetTable?:
             <Dialog open={showQRLink} onOpenChange={setShowQRLink}>
               <DialogTrigger asChild>
                 <Button size="icon" title="Compartir enlace de invitación" variant="outline">
-                  <QrCode className="h-4 w-4" />
+                  <QrCode />
                 </Button>
               </DialogTrigger>
               <DialogContent className="sm:max-w-[425px]">
@@ -201,23 +214,35 @@ function TableComponent({ table, resetTable }: { table: PokerTable; resetTable?:
               onClick={handleShareLink}
             >
               {!linkCopied ? (
-                <LinkIcon className="h-4 w-4 text-violet-500" />
+                <LinkIcon className="text-violet-500" />
               ) : (
-                <Check className="h-4 w-4 text-green-500" />
+                <Check className="text-green-500" />
               )}
             </Button>
-            <Link
-              className="flex items-center rounded border border-input p-1 text-amber-500 hover:bg-gray-800"
-              href="./fichas"
+            <a
+              className="flex items-center rounded border border-input px-1.5 text-gray-500 hover:bg-gray-800"
+              href="https://github.com/AugustoConti/pokereros"
             >
-              <CircleDotDashed />
-            </Link>
+              <GitHub />
+            </a>
             <Link
-              className="flex items-center rounded border border-input p-1 text-rose-500 hover:bg-gray-800"
+              className="flex items-center rounded border border-input px-1.5 hover:bg-gray-800"
               href="./manos"
             >
-              <span className="text-xl font-bold">A</span>
-              <Heart fill="currentColor" />
+              <div>
+                <Heart className="size-3 text-red-500" fill="currentColor" />
+                <Diamond className="size-3 text-blue-500" fill="currentColor" />
+              </div>
+              <div>
+                <Club className="size-3 text-green-500" fill="currentColor" />
+                <Spade className="size-3 text-white" />
+              </div>
+            </Link>
+            <Link
+              className="flex items-center rounded border border-input px-1.5 hover:bg-gray-800"
+              href="./fichas"
+            >
+              <CircleDotDashed className="text-amber-500" />
             </Link>
           </div>
         </header>
